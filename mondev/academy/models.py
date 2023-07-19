14859e5064d90ab0baa2d8cfae6c9718b6fcd355
Kestutis Januskevicius <infohata@gmail.com>
2023-07-20 01:50:05 +0300

    moved abstract models to monda_base

--- mondev/academy/models.py
+++ mondev/academy/models.py
@@ -1,45 +1,12 @@
 from datetime import timedelta
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from tinymce.models import HTMLField
+from monda_base.models import CodeNamedModel, NamedModel, TimeTrackedModel
 
 User = get_user_model()
-# TODO: move abstract models to monda_base app
 # TODO: refactor project to monda_live, mondev_ prefixes to monda_
-
-class TimeTrackedModel(models.Model):
-    created_at = models.DateTimeField(_("created at"), auto_now_add=True, db_index=True)
-    updated_at = models.DateTimeField(_("updated at"), auto_now=True, db_index=True)    
-
-    def get_absolute_url(self):
-        try:
-            url = reverse(f"{self.__class__.__name__.lower()}_detail", kwargs={"pk": self.pk})
-        except Exception as e:
-            print(e)
-        else:
-            return url 
-
-    class Meta:
-        abstract = True
-
-
-class NamedModel(TimeTrackedModel):
-    name = models.CharField(_("name"), max_length=127, db_index=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        abstract = True
-
-
-class CodeNamedModel(NamedModel):
-    code = models.SlugField(_("code"), max_length=7, db_index=True, null=True, blank=True)
-
-    class Meta:
-        abstract = True
 
 
 class Course(CodeNamedModel):
--- mondev/monda_base/models.py
+++ mondev/monda_base/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class TimeTrackedModel(models.Model):
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True, db_index=True)    
+
+    def get_absolute_url(self):
+        try:
+            url = reverse(f"{self.__class__.__name__.lower()}_detail", kwargs={"pk": self.pk})
+        except Exception as e:
+            print(e)
+        else:
+            return url 
+
+    class Meta:
+        abstract = True
+
+
+class NamedModel(TimeTrackedModel):
+    name = models.CharField(_("name"), max_length=127, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+class CodeNamedModel(NamedModel):
+    code = models.SlugField(_("code"), max_length=7, db_index=True, null=True, blank=True)
+
+    class Meta:
+        abstract = True
