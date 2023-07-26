b8c7d7774b52b41a5d04b9bb982458c6c425b819
kobrius11 <kobrius11@gmail.com>
2023-07-26 15:17:36 +0300

    email verification templates

--- monda_live/monda/settings.py
+++ monda_live/monda/settings.py
@@ -184,9 +184,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = local_settings.EMAIL_ID
 EMAIL_HOST_PASSWORD = local_settings.EMAIL_PW
 DEFAULT_FROM_EMAIL = local_settings.DEFAULT_FROM_EMAIL
-LOGIN_URL = '/accounts/login/'
+LOGIN_URL = 'login'
 
-VERIFICATION_SUCCESS_TEMPLATE = 'verify_email/email_verification_successful.html'
-VERIFICATION_FAILED_TEMPLATE = 'verify_email/email_verification_failed.html'
+
+# VERIFICATION_FAILED_TEMPLATE = 'verify_email/email_verification_failed.html'
 VERIFICATION_SUCCESS_MSG = """Your Email is verified successfully and account has been activated.
 You can login with the credentials now..."""
