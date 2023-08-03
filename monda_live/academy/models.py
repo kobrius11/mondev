149fb775cd8b3321234402a095e0540ff66d1f7e
kobrius11 <kobrius11@gmail.com>
2023-08-03 17:34:36 +0300

    begging of 37

--- monda_live/academy/models.py
+++ monda_live/academy/models.py
@@ -147,3 +147,38 @@ class TopicMaterial(NamedModel):
 
 class TopicMaterialTranslation(TopicMaterial, TranslatedModel):
     topic_material = models.ForeignKey(TopicMaterial, verbose_name=_("topic material"), on_delete=models.CASCADE, related_name='trabslations')
+
+
+## please review regarding issue 37
+# class CourseGroupSession(NamedModel):
+#     course_group = models.ForeignKey(
+#         CourseGroup, 
+#         verbose_name=_("course group"), 
+#         related_name="course_groups", 
+#         on_delete=models.CASCADE
+#     )
+#     course_topic = models.ForeignKey(
+#         CourseTopic, 
+#         verbose_name=_("course topic"), 
+#         related_name="course_topics", 
+#         on_delete=models.CASCADE
+#     )
+#     date = models.DateField(_("date"), auto_now=False, auto_now_add=False)
+#     stream_url = models.URLField(_("stream url"), null=True, max_length=200)#
+
+
+# class Attendence(models.Model):
+#     course_group_member = models.ForeignKey(
+#         CourseGroupMember, 
+#         verbose_name=_("course group member"), 
+#         related_name= "course_group_members",
+#         on_delete=models.CASCADE
+#     )
+#     course_group_session = models.ForeignKey(
+#         CourseGroupSession, 
+#         verbose_name=_("course group session"), 
+#         related_name= "course_group_sessions",
+#         on_delete=models.CASCADE
+#     )
+#     check_in = models.DateTimeField(_("check in"))#
+#     check_out = models.DateTimeField(_("check out"))#
