9d338490acb5e4318b516eeeb922008dfa516845
RenaldasZ <renald.zvega@gmail.com>
2023-08-21 13:20:55 +0300

    translated title, fixed transparency of burger and fixed ui.js

--- monda_live/mondev_site/static/mondev_site/js/ui.js
+++ monda_live/mondev_site/static/mondev_site/js/ui.js
@@ -23,9 +23,9 @@ async function monda_collapse(obj_id) {
 async function monda_uncollapse(obj_id) {
     const data = await json_get(sidebar_state_open_url);
     if (data.state == 1) {
-        var collapsible = document.getElementById(obj_id + "-content");
-        var collapse_button = document.getElementById(obj_id + "-collapse");
-        var uncollapse_button = document.getElementById(obj_id + "-uncollapse");
+        const collapsible = document.getElementById(obj_id + "-content");
+        const collapse_button = document.getElementById(obj_id + "-collapse");
+        const uncollapse_button = document.getElementById(obj_id + "-uncollapse");
         collapsible.classList.remove("hidden");
         collapse_button.classList.remove("hidden");
         collapse_button.classList.add("block");
@@ -35,7 +35,7 @@ async function monda_uncollapse(obj_id) {
 };
 
 async function active_button(obj_id) {
-    var uncollapsible = document.getElementById(obj_id);
+    const uncollapsible = document.getElementById(obj_id);
     if (uncollapsible.classList.contains("active")) {
         uncollapsible.classList.remove("active");
     } else {
